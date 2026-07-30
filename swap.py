869c6f1ed996258f4affc4873abdd4884bd7e737
Bhavana-2328 <bhavanareddy2328@gmail.com>
2026-07-30 13:05:17 +0530

    hi

---
+++ swap.py
@@ -0,0 +1,6 @@
+a = int(input("enter a number:"))
+b = int(input("enter a number:"))
+c = a #10
+a = b #20
+b = c #10
+print(a,b)
---
+++ swap1.py
@@ -0,0 +1,6 @@
+a = int(input("enter a number:"))
+b = int(input("enter a number:"))
+b = a + b #30
+a = b - a #20
+b = b - a #10
+print(a,b)
