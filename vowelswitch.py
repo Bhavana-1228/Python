2c828bd8ad57ea6f89d14a650214e09c38648996
CHINNAOBAIAHGARI BHAVANA REDDY <bhavanareddy2328@gmail.com>
2026-09-08 21:52:55 +0530

    hi

---
+++ vowelswitch.py
@@ -0,0 +1,14 @@
+ch = input("Enter a character: ")
+match ch:
+    case 'a':
+        print("Vowel")
+    case 'e':
+        print("Vowel")
+    case 'i':
+        print("Vowel")
+    case 'o':
+        print("Vowel")
+    case 'u':
+        print("Vowel")
+    case _:
+        print("Not a vowel")
