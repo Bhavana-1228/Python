176212204994c044c9de421a91c5d6edd4c2dd80
CHINNAOBAIAHGARI BHAVANA REDDY <bhavanareddy2328@gmail.com>
2026-09-08 21:58:40 +0530

    hi

---
+++ season.py
@@ -0,0 +1,28 @@
+month = int(input("Enter month number: "))
+match month:
+    case 1:
+        print("Winter")
+    case 2:
+        print("Winter")
+    case 3:
+        print("Summer")
+    case 4:
+        print("Summer")
+    case 5:
+        print("Summer")
+    case 6:
+        print("Rainy")
+    case 7:
+        print("Rainy")
+    case 8:
+        print("Rainy")
+    case 9:
+        print("Spring")
+    case 10:
+        print("Spring")
+    case 11:
+        print("Spring")
+    case 12:
+        print("Winter")
+    case _:
+        print("Invalid month")
